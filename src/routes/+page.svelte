e4514d29e158a8ef688f86b70a5e7c55103e9f59
andpav <pavlenkoandreyspb@gmail.com>
2023-10-05 17:50:44 +0400

    added svelte-forms-lib and yup validations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,92 @@
 <script lang="ts">
+    import { createForm } from 'svelte-forms-lib';
+    import { object, string } from 'yup';
+
     import 'modern-normalize/modern-normalize.css';
 
     export let name = 'kek';
 
-    let cardNumber = '';
-    let cardholderName = '';
-    let expireDate = '';
-    let cvv = '';
+    const CARD_NUMBER_MIN_LENGTH = 16;
+    const CARD_NUMBER_MAX_LENGTH = 18;
 
-    const submit = () => {
-        const result = {
-            cardNumber,
-            cardholderName,
-            expireDate,
-            cvv
+    const CVV_LENGTH = 3;
+
+    const { form, errors, state, handleChange, handleSubmit } = createForm({
+        initialValues: {
+            cardNumber: "",
+            cardholderName: "",
+            expireDate: "",
+            cvv: "",
+        },
+        validationSchema: object().shape({
+            cardNumber: string().min(CARD_NUMBER_MIN_LENGTH).max(CARD_NUMBER_MAX_LENGTH).required(),
+            cardholderName: string().matches().required(),
+            expireDate: string().required(),
+            cvv: string().length(CVV_LENGTH).required(),
+        }),
+        onSubmit: values => {
+            console.log(JSON.stringify(values));
         }
-
-        console.log(result, ' !!!!!!')
-    }
+    });
 </script>
 
 <main>
     <h1>Hello {name}!</h1>
 
     <div class="form-wrapper">
-        <input bind:value={cardNumber} placeholder="Card number"/>
-        <input bind:value={cardholderName} placeholder="Cardholder Name"/>
-        <input bind:value={expireDate} placeholder="Expiration Date"/>
-        <input bind:value={cvv} placeholder="CVV"/>
+        <input
+                id="cardNumber"
+                name="cardNumber"
+                placeholder="Card number"
+                on:change={handleChange}
+                on:blur={handleChange}
+                bind:value={$form.cardNumber}
+        />
 
-        <button on:click={submit}>submit</button>
+        {#if $errors.cardNumber}
+            <small>{$errors.cardNumber}</small>
+        {/if}
+
+        <input
+                id="cardholderName"
+                name="cardholderName"
+                placeholder="Cardholder Name"
+                on:change={handleChange}
+                on:blur={handleChange}
+                bind:value={$form.cardholderName}
+        />
+
+        {#if $errors.cardholderName}
+            <small>{$errors.cardholderName}</small>
+        {/if}
+
+        <input
+                id="expireDate"
+                name="expireDate"
+                placeholder="Expiration Date"
+                on:change={handleChange}
+                on:blur={handleChange}
+                bind:value={$form.expireDate}
+        />
+
+        {#if $errors.expireDate}
+            <small>{$errors.expireDate}</small>
+        {/if}
+
+        <input
+                id="cvv"
+                name="cvv"
+                placeholder="CVV"
+                on:change={handleChange}
+                on:blur={handleChange}
+                bind:value={$form.cvv}
+        />
+
+        {#if $errors.cvv}
+            <small>{$errors.cvv}</small>
+        {/if}
+
+        <button on:click={handleSubmit}>submit</button>
     </div>
 </main>
 
